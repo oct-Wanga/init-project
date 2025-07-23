60ae996d8a4bd12d9dfcc895b33270b24f96588b
jmwang <jeemin92@datasolution.kr>
2025-07-23 15:52:48 +0900

    Chore: 버튼 종류 기본값 수정

--- src/app/_component/common/button/Button.tsx
+++ src/app/_component/common/button/Button.tsx
@@ -6,38 +6,51 @@ import styles from './Button.module.css';
 
 const cx = classNames.bind(styles);
 
-type BtnSize = 'h-24' | 'h-32' | 'h-38' | 'h-40' | 'h-46' | 'none' | 'full';
+type BtnSize = 'h-24' | 'h-28' | 'h-32' | 'h-36' | 'h-38' | 'h-40' | 'h-48' | 'h-56' | 'auto';
+export type BtnColor = 'secondary' | 'tertiary' | 'transparent' | 'none';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size: BtnSize;
-  variant: 'primary' | 'secondary' | 'tertiary' | 'transparent' | 'annotation';
+  variant: BtnColor;
   children?: React.ReactNode;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
   loading?: boolean;
-  active?: boolean;
 }
 
 function Loading({ size }: { size: BtnSize }) {
   const getSize = () => {
     // 버튼 크기에 따라 loading 이미지 size 수정
-    if (size === 'h-24') {
-      return 20;
+    switch (size) {
+      case 'h-48':
+        return 28;
+      case 'h-40':
+      case 'h-38':
+        return 24;
+      case 'h-36':
+      case 'h-32':
+        return 20;
+      case 'h-28':
+        return 16;
+      case 'h-24':
+        return 14;
+      default:
+        return 24;
     }
-    return 24;
   };
 
-  return <SVGIcon className="" size={getSize()} />;
+  return (
+    <div className={cx('loading')}>
+      <SVGIcon name="icon-loading" size={getSize()} />
+    </div>
+  );
 }
 
 function Button(
-  { variant, size, children, startIcon, endIcon, loading, active, ...props }: ButtonProps,
+  { variant, size, children, startIcon, endIcon, loading, ...props }: ButtonProps,
   ref: ForwardedRef<HTMLButtonElement>,
 ) {
-  const classname = cx('btn', props.className, variant, size, {
-    loading,
-    active,
-  });
+  const classname = cx('btn', props.className, variant, size, { loading });
 
   return (
     <button ref={ref} {...props} className={classname}>
--- src/app/_component/common/button/IconButton.tsx
+++ src/app/_component/common/button/IconButton.tsx
@@ -1,38 +1,46 @@
 import React, { ButtonHTMLAttributes, ForwardedRef } from 'react';
 import classNames from 'classnames/bind';
+import { BtnColor } from '@_component/common/button/Button';
 import Tippy from '@tippyjs/react';
+
+import SVGIcon from '../SVGIcon';
 
 import styles from './IconButton.module.css';
 
 const cx = classNames.bind(styles);
 
-export type BtnSize = 'w-48' | 'w-42' | 'w-40' | 'w-32' | 'w-24' | 'w-20';
-export type IconVariant = 'secondary' | 'tertiary' | 'transparent';
+export type BtnSize = 'w-24' | 'w-40' | 'w-42' | 'auto';
 
 interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
   btnSize: BtnSize;
-  variant: IconVariant;
+  variant: BtnColor;
   tippyPlacement?: 'top' | 'bottom' | 'left' | 'right';
   tippyArrow?: boolean;
   tippyClassName?: string;
-  label?: string;
+  label?: string | React.ReactNode;
+  loading?: boolean;
 }
 
-function IconButton(
-  {
+function Loading({ size }: { size: BtnSize }) {
+  const getSize = () => Number(size.replace('w-', ''));
+  return <SVGIcon name="icon-loading" size={getSize()} />;
+}
+
+function IconButton(props: IconButtonProps, ref: ForwardedRef<HTMLButtonElement>) {
+  const {
     icon,
-    btnSize,
-    variant,
+    btnSize = 'w-40',
+    variant = 'tertiary',
     tippyPlacement = 'top',
-    tippyArrow = false,
+    tippyArrow = true,
     tippyClassName,
     label,
-    ...props
-  }: IconButtonProps,
-  ref: ForwardedRef<HTMLButtonElement>,
-) {
-  const classname = cx('icon-btn', btnSize, variant, props.className);
+    loading = false,
+    className,
+    ...rest
+  } = props;
+  const classname = cx('icon-btn', btnSize, variant, className, { loading });
 
   return (
     <Tippy
@@ -43,8 +51,8 @@ function IconButton(
       theme="default"
       className={tippyClassName}
     >
-      <button {...props} className={classname} ref={ref}>
-        {icon}
+      <button {...rest} className={classname} ref={ref}>
+        {loading ? <Loading size={btnSize} /> : icon}
       </button>
     </Tippy>
   );
